53dc69a4d4c25fb3c579dad68a95efe3a1d3c6fc
Ganokporn <ganokporn_pongdi@cmu.ac.th>
2022-12-20 14:45:51 +0700

    Add existing file

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
  
 app = Flask(__name__, static_folder='static')
  
-app.debug = True
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = '7e84a023e7357c07b10d8c32fd6e7e33deaee645c3234b8c'
  
 from app import views  # noqa
