aba71e357348f392adbe27424463f3808ae0be0b
Ganokporn <ganokporn_pongdi@cmu.ac.th>
2023-01-17 15:36:32 +0700

    Add existing file

--- app/__init__.py
+++ app/__init__.py
@@ -4,5 +4,6 @@ app = Flask(__name__, static_folder='static')
  
 app.config['DEBUG'] = True
 app.config['SECRET_KEY'] = '7e84a023e7357c07b10d8c32fd6e7e33deaee645c3234b8c'
+app.config['JSON_AS_ASCII'] = False
  
 from app import views  # noqa
